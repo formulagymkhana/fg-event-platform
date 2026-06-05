93b9c9c821eac1a062599e623c9bfa88bdd951ac
FormulaGymkhana <daiki.kikutake.msc@gmail.com>
2026-06-05 12:05:29 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@
 
 const FG_CONFIG = {
   // GASデプロイURL(変更時はここだけ書き換える)
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbwdXCvSIcv9n9LtwqqDHSTV1bYY_Jqsc1GzZu1bjBkoPD2b8gHkEVt54flboJuJYpSVNrg/exec',
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbwdXCvSIcv9n9LtwqqDHSTV1bYY_JqscGzZu1bjBkoPD2b8gHkEVt54flboJuJYpSVNrg/exec',
 
   // 現在のイベントID(設定シートのeventIdと一致させる)
   EVENT_ID: '2026_rd2',
